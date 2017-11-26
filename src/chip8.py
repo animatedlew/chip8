66c3ab8495931f98fc717c2d4290c1bb1707be88
Lewis Moronta <lewis.moronta@gmail.com>
2017-11-26 18:03:10 -0500

    + chip8: add SNE, JP, & RND

--- src/chip8.py
+++ src/chip8.py
@@ -4,6 +4,7 @@
 from cpu import CPU
 from rom import ROM
 import video
+import random
 
 cpu = CPU()
 
@@ -36,41 +37,48 @@ class Chip8:
             vy = 'V%X' % ((op & 0x00F0) >> 4)
 
             # execute
-            if group == 0:
+            if group == 0x00:
                 if op == 0x00E0: # CLS
                     video.clear()
                 elif op == 0x00EE: # RET
                     cpu.popPC()
-            elif group == 1: # JP
+
+            elif group == 0x01: # JP
                 cpu.R['PC'] = address
                 continue
-            elif group == 2: # CALL nnn
+
+            elif group == 0x02: # CALL nnn
                 cpu.pushPC()
                 cpu.R['PC'] = address
                 continue
-            elif group == 3: # SE Vx, kk
+
+            elif group == 0x03: # SE Vx, kk
                 # decode 3xkk, skip next instruction if Vx = kk.
                 if cpu.R[vx] == lsb:
                     cpu.R['PC'] += 2
                     continue
-            elif group == 4: # SNE Vx, kk
+
+            elif group == 0x04: # SNE Vx, kk
                 # decode 4xkk, skip next instruction if Vx != kk.
                 if cpu.R[vx] != lsb:
                     cpu.R['PC'] += 2
                     continue
-            elif group == 5: # SE Vx, Vy
+
+            elif group == 0x05: # SE Vx, Vy
                 # decode 5xy0, skip next instruction if Vx = Vy.
                 if cpu.R[vx] == cpu.R[vy]:
                     cpu.R['PC'] += 2
                     continue
-            elif group == 6: # LD Vx, kk
+
+            elif group == 0x06: # LD Vx, kk
                 # decode 6xkk, set Vx = kk.
                 cpu.R[vx] = lsb
-            elif group == 7: # ADD Vx, kk
+
+            elif group == 0x07: # ADD Vx, kk
                 # decode 7xkk - set Vx = Vx + kk.
                 cpu.R[vx] += lsb
 
-            elif group == 8:
+            elif group == 0x08:
                 kind = op & 0x000F
 
                 if (kind == 0):     # 8xy0 - LD Vx, Vy
@@ -98,6 +106,38 @@ class Chip8:
                     cpu.R['VF'] = 1 if cpu.R[vx] & 0x80 == 1 else 0
                     cpu.R[vx] *= 2
 
+            elif group == 0x09: # 9xy0 - SNE Vx, Vy
+                # decode 9xy0, skip next instruction if Vx != Vy.
+                if cpu.R[vx] != cpu.R[vy]:
+                    cpu.R['PC'] += 2
+                    continue
+
+            elif group == 0x0A: # Annn - LD I, addr
+                cpu.R['I'] = address
+
+            elif group == 0x0B: # Bnnn - JP V0, addr
+                cpu.R['PC'] = cpu.R['V0'] + address
+                continue
+
+            elif group == 0x0C: # Cxkk - RND Vx, kk
+                cpu.R[vx] = random.randint(0, 255) & lsb
+
+            elif group == 0x0D: # Dxyn - DRW Vx, Vy, nibble
+                nibble = op & 0x000F
+                '''
+                Display n-byte sprite starting at memory
+                location I at (Vx, Vy), set VF = collision.
+
+                The interpreter reads n bytes from memory,
+                starting at the address stored in I. These
+                bytes are then displayed as sprites on screen
+                at coordinates (Vx, Vy). Sprites are XORed onto
+                the existing screen. If this causes any pixels
+                to be erased, VF is set to 1, otherwise it is
+                set to 0. If the sprite is positioned so part
+                of it is outside the coordinates of the display,
+                it wraps around to the opposite side of the screen.
+                '''
             else:
                 return op
 
